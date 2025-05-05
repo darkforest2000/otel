0d317610641beeef6fe23485b6dc105cce9f6471
Egor <e.poletaykin@vkteam.ru>
2025-05-05 18:44:20 +0300

    fix bugs with click

--- clickhouse-init.sql
+++ clickhouse-init.sql
@@ -3,20 +3,28 @@ CREATE DATABASE IF NOT EXISTS otel_logs;
 CREATE TABLE IF NOT EXISTS otel_logs.logs
 (
     Timestamp DateTime64(9) CODEC(Delta, ZSTD(1)),
-    traceID String CODEC(ZSTD(1)),
-    spanID String CODEC(ZSTD(1)),
-    traceFlags UInt32 CODEC(ZSTD(1)),
-    severityText String CODEC(ZSTD(1)),
-    severityNumber UInt8 CODEC(ZSTD(1)),
-    body String CODEC(ZSTD(1)),
-    resource_attributes Map(String, String) CODEC(ZSTD(1)),
-    attributes Map(String, String) CODEC(ZSTD(1)),
+    TraceId String CODEC(ZSTD(1)),
+    SpanId String CODEC(ZSTD(1)),
+    TraceFlags UInt32 CODEC(ZSTD(1)),
+    SeverityText String CODEC(ZSTD(1)),
+    SeverityNumber UInt8 CODEC(ZSTD(1)),
+    Body String CODEC(ZSTD(1)),
+    ResourceAttributes Map(String, String) CODEC(ZSTD(1)),
+    Attributes Map(String, String) CODEC(ZSTD(1)),
+    ResourceSchemaUrl String CODEC(ZSTD(1)),
+    ScopeName String CODEC(ZSTD(1)),
+    ScopeVersion String CODEC(ZSTD(1)),
+    ScopeAttributes Map(LowCardinality(String), String) CODEC(ZSTD(1)),
+    ScopeDroppedAttrCount UInt32 CODEC(ZSTD(1)),
+    ScopeSchemaUrl String CODEC(ZSTD(1)),
+    ServiceName LowCardinality(String) CODEC(ZSTD(1)),
+    LogAttributes Map(LowCardinality(String), String) CODEC(ZSTD(1)),
     INDEX idx_timestamp Timestamp TYPE bloom_filter GRANULARITY 3,
-    INDEX idx_trace_id traceID TYPE bloom_filter GRANULARITY 3,
-    INDEX idx_severity_number severityNumber TYPE minmax GRANULARITY 3
+    INDEX idx_trace_id TraceId TYPE bloom_filter GRANULARITY 3,
+    INDEX idx_severity_number SeverityNumber TYPE minmax GRANULARITY 3
 ) ENGINE = MergeTree()
 PARTITION BY toDate(Timestamp)
-ORDER BY (Timestamp, severityNumber, traceID)
+ORDER BY (Timestamp, SeverityNumber, TraceId)
 TTL toDate(Timestamp) + INTERVAL 30 DAY
 SETTINGS index_granularity = 8192, ttl_only_drop_parts = 1;
 
@@ -24,14 +32,14 @@ SETTINGS index_granularity = 8192, ttl_only_drop_parts = 1;
 CREATE TABLE IF NOT EXISTS otel_logs.metrics
 (
     Timestamp DateTime64(9) CODEC(Delta, ZSTD(1)),
-    name String CODEC(ZSTD(1)),
-    description String CODEC(ZSTD(1)),
-    value Float64 CODEC(ZSTD(1)),
-    resource_attributes Map(String, String) CODEC(ZSTD(1)),
-    attributes Map(String, String) CODEC(ZSTD(1))
+    Name String CODEC(ZSTD(1)),
+    Description String CODEC(ZSTD(1)),
+    Value Float64 CODEC(ZSTD(1)),
+    ResourceAttributes Map(String, String) CODEC(ZSTD(1)),
+    Attributes Map(String, String) CODEC(ZSTD(1))
 ) ENGINE = MergeTree()
 PARTITION BY toDate(Timestamp)
-ORDER BY (Timestamp, name)
+ORDER BY (Timestamp, Name)
 TTL toDate(Timestamp) + INTERVAL 30 DAY
 SETTINGS index_granularity = 8192;
 
